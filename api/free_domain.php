793f02c73e336301684c7249e25f8af672d88bc6
amoc20 <moc.ales@seznam.cz>
2023-04-26 00:11:26 +0200

    free_domain.php done

--- api/free_domain.php
+++ api/free_domain.php
@@ -11,7 +11,25 @@
 <?php
     $domain = $_POST['domain'];
 
-    
+    $shell_format = "if test -d /etc/apache2/sites-available/%s_%s.conf; then echo 'exists'; fi";
 
-    return json_encode(array("name"=>$is_success, "credentials"=>$credentials));
+    $cz_exists = shell_exec(sprintf($shell_format, $domain, "cz"));
+    $com_exists = shell_exec(sprintf($shell_format, $domain, "com"));
+    $org_exists = shell_exec(sprintf($shell_format, $domain, "org"));
+
+    $free_domains = array();
+
+    if(strcmp($cz_exists, "exists") != 0) {
+        array_push($free_domains, array("name"=>$domain + ".cz", "cost"=>99));
+    }
+
+    if(strcmp($com_exists, "exists") != 0) {
+        array_push($free_domains, array("name"=>$domain + ".com", "cost"=>299));
+    }
+
+    if(strcmp($org_exists, "exists") != 0) {
+        array_push($free_domains, array("name"=>$domain + ".org", "cost"=>199));
+    }
+
+    return json_encode($free_domains);
 ?>
