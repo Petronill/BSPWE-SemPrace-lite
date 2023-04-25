5adb8aab957f62c6c54774f58490f7b864262708
Petr Čech <45454319+Petronill@users.noreply.github.com>
2023-04-25 19:18:45 +0200

    Delete upload_files.php

--- api/upload_files.php
+++
@@ -1,4 +0,0 @@
-// TODO:
-// přijde POST request s polem "domain" = string se jménem domény (s koncovkou) a "files[]" = polem souborů
-// nahraj soubory z files[] na danou doménu
-/* vrátí JSON objekt { 'success': true/false }*/
