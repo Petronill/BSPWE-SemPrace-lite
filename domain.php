08123ac1994c46e82a0c79040230ea15d862072f
Petr Čech <Petr ech@LAPTOP-5T240PPN>
2023-04-25 19:24:13 +0200

    Prepared.

--- domain.php
+++ domain.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 
 <body>
@@ -71,7 +71,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.3/umd/popper.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
-    <script src="js/ajaptor.js"></script>
-    <script src="js/dialog.js"></script>
-    <script src="js/domain.js"></script>
+    <script src="/js/ajaptor.js"></script>
+    <script src="/js/dialog.js"></script>
+    <script src="/js/domain.js"></script>
 </body>
